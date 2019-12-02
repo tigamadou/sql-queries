4b0c5e98a06801c9a4241d408ac51c27eab15d0a
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:34:37 +0100

    Show the winners with first name John

--- 3-select_from_nobel/queries.sql
+++ 3-select_from_nobel/queries.sql
@@ -8,4 +8,6 @@ SELECT yr, subject, winner  FROM nobel  WHERE yr = 1950
 
  SELECT yr, subject, winner FROM nobel WHERE subject ='Literature' AND yr>= 1980 AND yr<= 1989
 
- SELECT * FROM nobel  WHERE winner = 'Theodore Roosevelt' OR winner = 'Woodrow Wilson' OR winner = 'Jimmy Carter' OR winner = 'Barack Obama';
+ SELECT * FROM nobel  WHERE winner = 'Theodore Roosevelt' OR winner = 'Woodrow Wilson' OR winner = 'Jimmy Carter' OR winner = 'Barack Obama'
+
+ SELECT winner FROM nobel  WHERE winner LIKE 'John%';
