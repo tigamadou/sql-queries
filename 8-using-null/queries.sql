d8e21d0add72e42a6874849757226ddf70ddb104
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 13:37:29 +0100

    Using null sql tutorials 5 to 7 done

--- 8-using-null/queries.sql
+++ 8-using-null/queries.sql
@@ -6,3 +6,9 @@ SELECT teacher.name, dept.name FROM teacher LEFT JOIN dept ON (teacher.dept=dept
 
 SELECT teacher.name, dept.name FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
 
+SELECT name, COALESCE(mobile,'07986 444 2266') FROM teacher
+
+SELECT teacher.name, COALESCE(dept.name,'None') FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+SELECT COUNT(name), COUNT(mobile) FROM teacher
+
