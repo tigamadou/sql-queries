12fc13b37910df7230f13c3cb506cb6abffd30fc
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 13:17:09 +0100

    join tutorials done

--- 6-join/queries.sql
+++ 6-join/queries.sql
@@ -19,3 +19,7 @@ SELECT teamname, COUNT(player) FROM eteam JOIN goal ON id=teamid GROUP BY teamna
 SELECT stadium, COUNT(player) FROM game JOIN goal ON id=matchid GROUP BY stadium
 
 SELECT matchid, mdate, COUNT(teamid) FROM game JOIN goal ON matchid = id WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY matchid, mdate
+
+SELECT matchid, mdate, COUNT(teamid) FROM game JOIN goal ON matchid = id WHERE(team1 = 'GER' OR team2 = 'GER') AND teamid = 'GER' GROUP BY matchid, mdate
+
+SELECT mdate, team1, SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) as score1, team2, SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END)as score2 FROM game LEFT JOIN goal ON matchid = id GROUP BY team1, mdate, team2, matchid ORDER BY mdate, matchid, team1, team2;
