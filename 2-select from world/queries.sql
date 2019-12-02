95cb30f78b224fa05fa8a717a784e53e7a7c8142
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:14:29 +0100

    Select from queries done

--- 2-select from world/queries.sql
+++ 2-select from world/queries.sql
@@ -18,3 +18,15 @@ SELECt name, (population/1000000) FROM world
 
   SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world 
   WHERE continent = 'South America'
+
+  SELECT name, ROUND(gdp/population, -3) FROM world 
+  WHERE gdp >= 1000000000000;
+
+SELECT name, capital FROM world
+  WHERE LENGTH(name) = LENGTH(capital);
+
+  SELECT name, capital FROM world 
+  WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital;
+
+  SELECT name FROM world
+  WHERE name LIKE '%a%' AND name LIKE '%e%'AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
