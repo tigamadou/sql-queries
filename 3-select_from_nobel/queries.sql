b43074d12b9cb5437e2c0eb190e31fa943c33553
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:32:59 +0100

    Show all details of the presidential winners:

--- 3-select_from_nobel/queries.sql
+++ 3-select_from_nobel/queries.sql
@@ -7,3 +7,5 @@ SELECT yr, subject, winner  FROM nobel  WHERE yr = 1950
  SELECT winner FROM nobel WHERE subject ='Peace' AND yr >= 2000
 
  SELECT yr, subject, winner FROM nobel WHERE subject ='Literature' AND yr>= 1980 AND yr<= 1989
+
+ SELECT * FROM nobel  WHERE winner = 'Theodore Roosevelt' OR winner = 'Woodrow Wilson' OR winner = 'Jimmy Carter' OR winner = 'Barack Obama';
