93b9e48afa63332107ca53fbbe05158115386939
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:28:33 +0100

    Give the name of the 'Peace' winners since the year 2000, including 2000.

--- 3-select_from_nobel/queries.sql
+++ 3-select_from_nobel/queries.sql
@@ -1,8 +1,7 @@
-SELECT yr, subject, winner
-  FROM nobel
- WHERE yr = 1950
+SELECT yr, subject, winner  FROM nobel  WHERE yr = 1950
 
- SELECT winner FROM nobel
- WHERE yr = 1962 AND subject = 'Literature';
+ SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature';
 
  SELECT yr,subject FROM nobel where winner ="Albert Einstein"
+
+ SELECT winner FROM nobel WHERE subject ='Peace' AND yr >= 2000
