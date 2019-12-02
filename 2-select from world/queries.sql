f01fd4884685656cf54271d3f2a1b5fca9ed9159
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:15:11 +0100

    Select from world queries done

--- 2-select from world/queries.sql
+++ 2-select from world/queries.sql
@@ -30,3 +30,5 @@ SELECT name, capital FROM world
 
   SELECT name FROM world
   WHERE name LIKE '%a%' AND name LIKE '%e%'AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
+ 
+ 
