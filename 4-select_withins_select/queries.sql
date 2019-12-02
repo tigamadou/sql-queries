980519ecd9227bc0060bdf2b78a5cf6eee8a260b
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:50:28 +0100

    Which country has a population that is more than Canada but less than Poland? Show the name and the population.

--- 4-select_withins_select/queries.sql
+++ 4-select_withins_select/queries.sql
@@ -5,3 +5,6 @@ SELECT name FROM world WHERE continent = 'Europe' AND gdp/population > (SELECT g
 SELECT name, continent FROM world WHERE continent = (SELECT continent FROM world WHERE name = 'Australia') OR continent = (SELECT continent FROM world 
 WHERE name = 'Argentina') ORDER BY name
 
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world 
+WHERE name = 'Poland'); 
+
