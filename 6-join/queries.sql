f9dd8948c47af78d6f3d96700ad5e91eb2a32f4a
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 13:15:33 +0100

    join tutorial 6 to 11 done

--- 6-join/queries.sql
+++ 6-join/queries.sql
@@ -7,3 +7,15 @@ SELECT player, teamid, stadium, mdate FROM game JOIN goal ON (id=matchid) WHERE 
 SELECT team1,team2, player FROM game JOIN goal ON (id=matchid) WHERE player LIKE 'Mario%'
 
 SELECT player, teamid, coach, gtime FROM goal JOIN eteam ON (teamid=id) WHERE gtime<=10
+
+SELECT mdate, teamname FROM game JOIN eteam ON (team1=eteam.id) WHERE coach =  'Fernando Santos'
+
+SELECT player FROM game JOIN goal ON (id=matchid) WHERE stadium =  'National Stadium, Warsaw'
+
+SELECT DISTINCT player FROM game JOIN goal ON matchid = id WHERE (team1='GER' OR team2='GER') AND teamid != 'GER'
+
+SELECT teamname, COUNT(player) FROM eteam JOIN goal ON id=teamid GROUP BY teamname
+
+SELECT stadium, COUNT(player) FROM game JOIN goal ON id=matchid GROUP BY stadium
+
+SELECT matchid, mdate, COUNT(teamid) FROM game JOIN goal ON matchid = id WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY matchid, mdate
