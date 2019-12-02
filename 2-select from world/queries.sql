529a041a98cd33450790d85333dd15d15f362400
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:09:33 +0100

    Show the countries that are big by area or big by population. Show name, population and area.

--- 2-select from world/queries.sql
+++ 2-select from world/queries.sql
@@ -1,2 +1,17 @@
 SELECT name FROM world
   WHERE population >= 200000000
+
+SELECT name, gdp/population FROM world 
+  WHERE population >= 200000000;
+
+SELECt name, (population/1000000) FROM world 
+  WHERE continent = 'South America';
+
+  SELECt name, population FROM world 
+  WHERE name IN ('France', 'Germany', 'Italy')
+
+  SELECT name FROM world 
+  WHERE name LIKE '%United%';
+
+  SELECT name, population, area FROM world 
+  WHERE area > 3000000 OR population > 250000000;
