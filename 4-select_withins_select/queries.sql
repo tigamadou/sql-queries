b4dc0bee6a6834532cd871698833c29e6f9ea66d
Amadou Ibrahim <amadou140589@gmail.com>
2019-12-02 12:48:12 +0100

    Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.

--- 4-select_withins_select/queries.sql
+++ 4-select_withins_select/queries.sql
@@ -1,1 +1,3 @@
 SELECT name FROM world WHERE population > (SELECT population FROM world WHERE name='Russia')
+
+SELECT name FROM world WHERE continent = 'Europe' AND gdp/population > (SELECT gdp/population FROM world WHERE name='United Kingdom');
